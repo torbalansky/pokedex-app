35966eafc3da77876e715681bc080794e6ee5281
torbalansky <torbalansky@gmail.com>
2023-02-10 15:01:07 +0000

    Add list of 5 pokemon with respective name, height and type

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert('Hello world');
-let favoriteFood = 'Lemon';
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Bulbasaur', height: '0.7', type: ['grass', 'poison']},
+    { name: 'Kakuna', height: '0.6', type: [ 'bug', 'poison']},
+    { name: 'Gengar', height: '1.5', type: ['ghost', 'poison']},
+    { name: 'Poliwhirl', height: '1', type: 'water' },
+    { name: 'Mankey', height: '0.5', type: 'fighting' },
+]
