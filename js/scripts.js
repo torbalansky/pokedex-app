934e4a7e8cdb6fb655220c72746648dd0daef898
torbalansky <torbalansky@gmail.com>
2023-02-12 17:21:51 +0000

    Create a for loop. Add a condition statement.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,29 @@
 let pokemonList = [
-    { name: 'Bulbasaur', height: '0.7', type: ['grass', 'poison']},
-    { name: 'Kakuna', height: '0.6', type: [ 'bug', 'poison']},
-    { name: 'Gengar', height: '1.5', type: ['ghost', 'poison']},
-    { name: 'Poliwhirl', height: '1', type: 'water' },
-    { name: 'Mankey', height: '0.5', type: 'fighting' },
+ // Initialize empty array   
+{ name: "Bulbasaur", height: 0.7, type: [ "grass", "poison" ] },
+    
+{ name: "Kakuna", height: 0.6, type: [ "bug", "poison" ] },
+    
+{ name: "Gengar", height: 1.5, type: [ "ghost", "poison" ] },
+
+{ name: 'Poliwhirl', height: '1', type: 'water' },
+
+{ name: 'Mankey', height: '0.5', type: 'fighting' },
+
 ]
+
+
+for (let i = 0; i < pokemonList.length; i++)
+// Initalize for loop to run for the total length of the array
+
+ { 
+    if (pokemonList[i].height > 1) {
+        console.log(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!");
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")  - Wow, that's big!" + "<br>");
+    } 
+    
+    else {
+        console.log(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" + "<br>");
+    }
+}
